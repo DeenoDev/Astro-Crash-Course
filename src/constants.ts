f6631eaf04ff593ed850697a95f41d8e294b4c59
DeenoDev <deyanoklaasen@outlook.com>
2024-05-24 23:55:18 +0200

    Added 2 more constants to constant.ts file.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'TechPeople Blog';
 export const SITE_DESCRIPTION = 'Articles, Stories and tutorials from TechPeople';
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
