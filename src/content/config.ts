d83c67f2f50e2203cacdc27e818f1e8ee1f3dbac
DeenoDev <deyanoklaasen@outlook.com>
2024-05-31 23:06:55 +0200

    blog collection schema completed.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,17 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        author: z.string(),
+        image: z.string(),
+        tags: z.array(z.string()),
 
-export const collections = {};
+    })
+});
+
+export const collections = {
+    blog: blogCollection,
+};
