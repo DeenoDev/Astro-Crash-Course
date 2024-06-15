ea2477ebb97bbb0f9e326c9204f699d4315c71ff
DeenoDev <deyanoklaasen@outlook.com>
2024-06-15 22:59:19 +0200

    turned GET method into async function.

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import type { CollectionEntry } from 'astro:content';
 
-export const GET: APIRoute = ({ url }): Promise<Response> => {
+export const GET: APIRoute = async ({ url }): Promise<Response> => {
    const query: string | null = url.searchParams.get('query');
 
    // Handle if query is not present
