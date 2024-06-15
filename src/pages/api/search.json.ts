12f6586953a7356705cad5c1ce78837f94ff7a57
DeenoDev <deyanoklaasen@outlook.com>
2024-06-15 22:27:10 +0200

    exported GET method function for retrieving query from url.

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -1,1 +1,6 @@
 import type { APIRoute } from "astro";
+
+export const GET: APIRoute = ({ url }): Response => {
+   const query: string | null = url.searchParams.get('query');
+   console.log(query); 
+};
