ea3d3328c44183506f5014e8d84f3ff9b764536a
DeenoDev <deyanoklaasen@outlook.com>
2024-06-15 22:34:26 +0200

    completed script to handle function if query is not present in GET method.

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -2,5 +2,16 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ url }): Response => {
    const query: string | null = url.searchParams.get('query');
-   console.log(query); 
+
+   // Handle if query is not present
+   if (query === null) {
+    return new Response(JSON.stringify({
+        error: 'Query param is missing'
+    }), {
+        status: 400, // Bad Request
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+   } 
 };
