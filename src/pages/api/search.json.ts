bddebb52b0899c926d74769a380326e62a5b4efc
DeenoDev <deyanoklaasen@outlook.com>
2024-06-15 22:47:23 +0200

    modified GET method to be asynchronous with the promise keyword and Response type.

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import type { CollectionEntry } from 'astro:content';
 
-export const GET: APIRoute = ({ url }): Response => {
+export const GET: APIRoute = ({ url }): Promise<Response> => {
    const query: string | null = url.searchParams.get('query');
 
    // Handle if query is not present
